28935629dadfa6a0122d2d74353ac7d3e4c9c66e
Arthur <arthur@jovart.com>
2020-01-20 20:39:29 +0000

    Ajout d'une contrainte sur les types de transport en base de données

--- GhostRunWeb/front/models.py
+++ GhostRunWeb/front/models.py
@@ -6,6 +6,23 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+TRANSPORT_MODES = [
+    ('walk', 'Walk'),
+    ('run', 'Run'),
+    ('car', 'Car'),
+    ('motorbike', 'Motorbike'),
+    ('taxi', 'Taxi'),
+    ('rideshare', 'Rideshare'),
+    ('carpool', 'Carpool'),
+    ('bus', 'Bus'),
+    ('bike', 'Bike'),
+    ('boat', 'Boat'),
+    ('train', 'Train'),
+    ('rer', 'RER'),
+    ('plane', 'Plane'),
+    ('kayak', 'Kayak')
+]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)  # Boulot-Maison, faire des courses, autour du campus....
@@ -15,8 +32,8 @@ class Trajet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # int (id d'user)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  # int (id de categorie)
     started_at = models.DateTimeField()  # 2020-03-30+17-16-03-9923+GMT1 heure de début
-    ended_at = models.DateTimeField() # 2020-03-30+17-16-03-9923+GMT1 heure de fin
-    transport_used = models.CharField(max_length=50)  # car, bike, boat, train, rer, bus, run, ....
+    ended_at = models.DateTimeField()  # 2020-03-30+17-16-03-9923+GMT1 heure de fin
+    transport_used = models.CharField(max_length=50, choices=TRANSPORT_MODES)  # car, bike, boat, train, rer, bus, run, ....
     # localisations = [{Localisations}]
 
 
