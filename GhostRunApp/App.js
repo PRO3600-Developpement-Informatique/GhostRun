a5f36670a0aee9fde760a74fe4e9f5cb276fcbb8
Arthur <arthur@jovart.com>
2020-03-12 18:56:42 +0100

    Suivi de la localisation et compteur de distance

--- GhostRunApp/App.js
+++ GhostRunApp/App.js
@@ -1,7 +1,32 @@
-import MapView from 'react-native-maps';
+import React from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Platform,
+    PermissionsAndroid
+} from "react-native";
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {request, PERMISSIONS} from 'react-native-permissions';
+
+import MapView, {
+    Marker,
+    AnimatedRegion,
+    Polyline,
+    PROVIDER_GOOGLE
+} from "react-native-maps";
+import haversine from "haversine";
+
+import Geolocation from 'react-native-geolocation-service';
+
+// const LATITUDE = 29.95539;
+// const LONGITUDE = 78.07513;
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+
 
 const mapStyle = [
     {
@@ -231,39 +256,158 @@ const mapStyle = [
             }
         ]
     }
-]
+];
 
 const styles = StyleSheet.create({
-    bigBlue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-    },
-    red: {
-        color: 'red',
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: "flex-end",
+        alignItems: "center"
     },
     map: {
-        ...StyleSheet.absoluteFillObject,
+        ...StyleSheet.absoluteFillObject
+    },
+    bubble: {
+        flex: 1,
+        backgroundColor: "rgba(255,255,255,0.7)",
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20
+    },
+    latlng: {
+        width: 200,
+        alignItems: "stretch"
+    },
+    button: {
+        width: 80,
+        paddingHorizontal: 12,
+        alignItems: "center",
+        marginHorizontal: 10
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginVertical: 20,
+        backgroundColor: "transparent"
     }
 });
 
 
-export default class GhostRunApp extends Component {
+export default class MainMapView extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            routeCoordinates: [],
+            distanceTravelled: 0,
+            prevLatLng: {},
+            coordinate: new AnimatedRegion({
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+                latitudeDelta: 0,
+                longitudeDelta: 0
+            })
+        };
+    }
+
+    componentDidMount() {
+        const { coordinate } = this.state;
+        if (Platform.OS === "android") {
+            request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(result => {
+                // aaa
+            });
+        } else {
+            request(PERMISSIONS.IOS.LOCATION_ALWAYS).then(result => {
+                // aaa
+            });
+        }
+
+        this.watchID = Geolocation.watchPosition(
+            position => {
+                const { routeCoordinates, distanceTravelled } = this.state;
+                const { latitude, longitude } = position.coords;
+
+                const newCoordinate = {
+                    latitude,
+                    longitude
+                };
+
+                if (Platform.OS === "android") {
+                    if (this.marker) {
+                        this.marker._component.animateMarkerToCoordinate(
+                            newCoordinate,
+                            500
+                        );
+                    }
+                } else {
+                    coordinate.timing(newCoordinate).start();
+                }
+
+                this.setState({
+                    latitude,
+                    longitude,
+                    routeCoordinates: routeCoordinates.concat([newCoordinate]),
+                    distanceTravelled:
+                        distanceTravelled + this.calcDistance(newCoordinate),
+                    prevLatLng: newCoordinate
+                });
+            },
+            error => console.log(error),
+            {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 1000,
+                distanceFilter: 10
+            }
+        );
+    }
+
+    componentWillUnmount() {
+        Geolocation.clearWatch(this.watchID);
+    }
+
+    getMapRegion = () => ({
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+    });
+
+    calcDistance = newLatLng => {
+        const { prevLatLng } = this.state;
+        return haversine(prevLatLng, newLatLng) || 0;
+    };
+
     render() {
         return (
-            // Try setting `justifyContent` to `center`.
-            // Try setting `flexDirection` to `row`.
-            <MapView
-                customMapStyle={mapStyle}
-                style={styles.map}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-
-            />
+            <View style={styles.container}>
+                <MapView
+                    customMapStyle={mapStyle}
+                    style={styles.map}
+                    provider={PROVIDER_GOOGLE}
+                    showUserLocation
+                    followUserLocation
+                    loadingEnabled
+                    region={this.getMapRegion()}
+                >
+                    <Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} />
+                    <Marker.Animated
+                        ref={marker => {
+                            this.marker = marker;
+                        }}
+                        coordinate={this.state.coordinate}
+                    />
+                </MapView>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={[styles.bubble, styles.button]}>
+                        <Text style={styles.bottomBarContent}>
+                            {parseFloat(this.state.distanceTravelled).toFixed(3 )} km
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         );
     }
 };
