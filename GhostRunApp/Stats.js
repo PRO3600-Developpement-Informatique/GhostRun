ff35134363c699570d3fe320b332f7e2b4037292
AnthonyLamarque <anthonylamarque@hotmail.com>
2020-03-16 12:33:37 +0100

    Ajout de fonctions pour le composant PageStats

--- GhostRunApp/Stats.js
+++ GhostRunApp/Stats.js
@@ -10,4 +10,32 @@ import {
 
     export default class PageStats extends React.Component {
 
+        temps_trajet(trajet){
+
+        }
+
+        distance_trajet(trajet){
+
+        }
+
+        vitesse_moy_trajet(trajet){
+
+        }
+
+        temps_max(trajet){
+
+        }
+
+        temps_min(trajet){
+
+        }
+
+        date_trajet(trajet){
+
+        }
+
+        afficher_trajet_sur_map(trajet){
+
+        }
+
     }
