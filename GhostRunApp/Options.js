df9ef45d1e064f55de148294194fda1b6413e01e
AnthonyLamarque <anthonylamarque@hotmail.com>
2020-03-16 12:27:32 +0100

    Initialisation PagesOptions

---
+++ GhostRunApp/Options.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Platform,
+    PermissionsAndroid
+} from "react-native";
+
+export default class PageOptions extends React.Component {
+
+}
