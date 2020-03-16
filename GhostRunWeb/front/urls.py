c9ef28bf82fd2a91341ead0066b65b848da7271f
Arthur <arthur@jovart.com>
2020-03-16 20:57:33 +0100

    Ajout des vues de base

--- GhostRunWeb/front/urls.py
+++ GhostRunWeb/front/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('page1', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('accounts/login', views.login_view, name='login'),
+    path('accounts/logout', views.logout_view, name='logout'),
+    path('accounts/register', views.register_view, name='register'),
+    path('trips', views.my_trips_view, name='my-trips'),
+    path('settings', views.my_settings_view, name='my-settings'),
+    path('trips/<trip_id:int>', views.trip_detail_view, name='trip-detail'),
 ]
--- GhostRunWeb/front/views.py
+++ GhostRunWeb/front/views.py
@@ -1,7 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("c'est la vue pour front, le site web")
+    return HttpResponse("Page d'index à creer")
+
+def login_view(request):
+    return HttpResponse("Page à creer")
+
+def logout_view(request):
+    return HttpResponse("Page à creer")
+
+def register_view(request):
+    return HttpResponse("Page à creer")
+
+@login_required()
+def my_trips_view(request):
+    return HttpResponse("Page à creer")
+
+@login_required()
+def my_settings_view(request):
+    return HttpResponse("Page à creer")
+
+@login_required()
+def trip_detail_view(request, trip_id:int):
+    return HttpResponse("Page à creer")
