a7e5f20e1ad47b16ef5ca05bbdfe58230068e1bc
Arthur <arthur@jovart.com>
2020-04-30 16:15:16 +0200

    Rename Trajets -> Trip

--- GhostRunWeb/front/admin.py
+++ GhostRunWeb/front/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, UserSettings, Trip, Localisations
+
+admin.site.register(Category)
+admin.site.register(UserSettings)
+admin.site.register(Trip)
+admin.site.register(Localisations)
--- GhostRunWeb/front/models.py
+++ GhostRunWeb/front/models.py
@@ -38,7 +38,7 @@ class Trip(models.Model):
 
 
 class Localisations(models.Model):
-    trajet = models.ForeignKey(Trajet, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
     altitude = models.FloatField()
