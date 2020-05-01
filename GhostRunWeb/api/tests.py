bfb1aa6015350f9d5ab3073037bd8d58c7f9c90b
Arthur <arthur@jovart.com>
2020-05-01 15:27:48 +0200

    Ajout de tests simples de l'API

--- GhostRunWeb/api/tests.py
+++ GhostRunWeb/api/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
-
 # Create your tests here.
+
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory, APIClient
+
+
+class APITests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(username='arthur', email='arthur@example.com', password='top_secret')
+
+    def test_api_needs_authentification(self):
+        c = APIClient()
+        api_root_url = reverse('api-root')
+        # Create an instance of a GET request.
+        r = c.get(api_root_url)
+        self.assertEqual(r.status_code, 403)
+
+    def test_api_works_authentified(self):
+        c = APIClient()
+        api_root_url = reverse('api-root')
+        c.force_login(self.user)
+        r = c.get(api_root_url)
+        self.assertEqual(r.status_code, 200)
+
+
--- GhostRunWeb/api/urls.py
+++ GhostRunWeb/api/urls.py
@@ -15,5 +15,5 @@ router.register(r'trips', views.TripViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='api'))
 ]
