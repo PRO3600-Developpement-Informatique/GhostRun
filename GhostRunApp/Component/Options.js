47ab2fb604714dde8cf47a55d829ed34cd3096d2
Arthur <arthur@jovart.com>
2020-05-02 14:52:49 +0200

    Revert "Initialisation de la page options"
    This reverts commit bbb38f03

--- GhostRunApp/Component/Options.js
+++ GhostRunApp/Component/Options.js
@@ -1,12 +1,4 @@
 import React from 'react';
-import ReactNativeSettingsPage, {
-  SectionRow,
-  NavigateRow,
-  CheckRow,
-  SwitchRow,
-  SliderRow,
-} from 'react-native-settings-page';
-
 import {
   StyleSheet,
   View,
@@ -16,46 +8,12 @@ import {
   PermissionsAndroid,
 } from 'react-native';
 export default class PageOptions extends React.Component {
-  state = {
-    check: false,
-    switch: false,
-    value: 40,
-  };
-
+  changer_style_map() {} // Change le theme de la carte mode jour->mode nuit ou inversement
   render() {
     return (
-      <ReactNativeSettingsPage>
-        <SectionRow text="Options">
-          <SwitchRow
-            text="Thème sombre"
-            iconName="your-icon-name"
-            _value={this.state.switch}
-            _onValueChange={() => {
-              this.setState({switch: !this.state.switch});
-            }}
-          />
-          <CheckRow
-            text="CheckRow"
-            iconName="your-icon-name"
-            _color="#000"
-            _value={this.state.check}
-            _onValueChange={() => {
-              this.setState({check: !this.state.check});
-            }}
-          />
-          <SliderRow
-            text="Slider Row"
-            iconName="your-icon-name"
-            _color="#000"
-            _min={0}
-            _max={100}
-            _value={this.state.value}
-            _onValueChange={value => {
-              this.setState({value});
-            }}
-          />
-        </SectionRow>
-      </ReactNativeSettingsPage>
+      <View>
+        <Text>Page des options !!</Text>
+      </View>
     );
   }
 }
