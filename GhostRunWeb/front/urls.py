d89b75db5df0af38e7c21e376c0ec07579ea0a3b
Arthur <arthur@jovart.com>
2020-05-13 00:26:29 +0200

    Ajout signup.html

--- GhostRunWeb/front/urls.py
+++ GhostRunWeb/front/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
     path('', views.index, name='index'),
     path('trips', views.my_trips_view, name='my-trips'),
     path('settings', views.my_settings_view, name='my-settings'),
--- GhostRunWeb/front/views.py
+++ GhostRunWeb/front/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
 # Create your views here.
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
 
 def index(request):
     return render(request, "front/index.html", context={})
