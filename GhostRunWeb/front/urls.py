bf50034028b40c5fd5d5122b6e3f8fbeac00f6ce
Arthur <arthur@jovart.com>
2020-05-13 00:14:05 +0200

    Ceci explique cela
    On bossait sur les mauvaises views.. .

--- GhostRunWeb/front/urls.py
+++ GhostRunWeb/front/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/login', views.login_view, name='login'),
-    path('accounts/logout', views.logout_view, name='logout'),
-    path('accounts/register', views.register_view, name='register'),
     path('trips', views.my_trips_view, name='my-trips'),
     path('settings', views.my_settings_view, name='my-settings'),
     path('trips/<int:trip_id>', views.trip_detail_view, name='trip-detail'),
