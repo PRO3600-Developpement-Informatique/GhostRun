016d50fdf07d6f23321a0a9a969f0430e6ac87e6
Arthur <arthur@jovart.com>
2020-05-14 11:05:47 +0200

    Changement des noms d'URL

--- GhostRunWeb/front/urls.py
+++ GhostRunWeb/front/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('accounts/signup/', views.SignUp.as_view(), name='signup'),
-    path('', views.index, name='index'),
-    path('me', views.profile_home, name='profile-home'),
-    path('settings', views.my_settings_view, name='my-settings'),
-    path('trips/<int:pk>/', views.TripDetail.as_view(), name='trip-detail'),
-    path('trips/<int:pk>/gpx', views.TripGPX.as_view(), name='trip-gpx'),
+    path('', views.index, name='front-index'),
+    path('me', views.profile_home, name='front-profile-home'),
+    path('settings', views.my_settings_view, name='front-my-settings'),
+    path('trips/<int:pk>/', views.TripDetail.as_view(), name='front-trip-detail'),
+    path('trips/<int:pk>/gpx', views.TripGPX.as_view(), name='front-trip-gpx'),
 ]
