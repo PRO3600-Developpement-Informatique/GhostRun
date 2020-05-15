37eeb63ddcdff89457804e2e8032dff3e57bfe97
Arthur <arthur@jovart.com>
2020-05-15 11:38:40 +0200

    Crispy forms pour l'index

--- GhostRunWeb/app/forms.py
+++ GhostRunWeb/app/forms.py
@@ -1,8 +1,10 @@
 import datetime
 
+from crispy_forms.helper import FormHelper
 from django import forms
 
 from front.models import Trip, User
+from crispy_forms_foundation.layout import Layout, Fieldset, ButtonHolder, Submit
 
 
 class CategoryChoiceField(forms.ModelChoiceField):
@@ -14,10 +16,20 @@ class AppInitForm(forms.ModelForm):
     def __init__(self, *args, user: User, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['category'].queryset = user.category_set.all()
+        self.helper = FormHelper()
+        self.helper.form_action = '.'
+        self.helper.layout = Layout(
+            Fieldset("{{ user.username|title }}, ou voulez-vous aller ?",
+                     'category',
+                     'transport_used',
+                     ),
+            ButtonHolder(
+                Submit('submit', "C'est parti!"),
+            ),
+        )
 
     category = CategoryChoiceField(queryset=None, empty_label=None)
 
     class Meta:
         model = Trip
-        fields = '__all__'
-        exclude = ['user', 'ended_at', 'weather', 'feeling', 'started_at']
+        fields = ['category', 'transport_used']
