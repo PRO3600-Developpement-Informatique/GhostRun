6316d67036f8c0226709b1dee2a6509adaa4752d
Arthur <arthur@jovart.com>
2020-06-01 16:19:46 +0200

    ✨ Ne pas auth par session

--- GhostRunWeb/GhostRunWeb/settings.py
+++ GhostRunWeb/GhostRunWeb/settings.py
@@ -131,6 +131,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'PAGE_SIZE': 100
 }
 
